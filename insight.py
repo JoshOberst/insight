eb5c93e759ff08742dca53ee3e76b9ff8c51f0db
JoshOberst <joshoberst11@gmail.com>
2022-03-04 15:03:51 -0500

    made returns more accurate

--- insight.py
+++ insight.py
@@ -4,19 +4,25 @@ import pandas as pd
 import datetime as dt
 from pathlib import Path
 import numpy as np
+from pypfopt import expected_returns
 
 
 startDay = dt.datetime(2018,12,31)
-stockList = []
+stockList = ['AAPL','TSLA','SPY']
+benchList = ['^IXIC'] #This can be changed to suit needs. ONLY 1 STOCK 
 
 
-data = yf.download(stockList,startDay).get("Adj Close")
+data = yf.download(stockList+benchList,startDay).get("Adj Close")
 returns = np.log(data.copy()).diff().dropna() #log returns
 
+assets = returns.loc[:, returns.columns != benchList[0]]
+benchmark = returns.loc[:, returns.columns == benchList[0]]
 
+eR = expected_returns.capm_return(assets,market_prices = benchmark, returns_data = True, risk_free_rate = 0.07/100,frequency = 252)
+print(eR)
 
-covMatrix = returns.reset_index(drop=True).cov()
-eR = (returns.copy().sum())/returns.size #daily
+covMatrix = assets.cov()*252
+print(covMatrix)
 
 filepath1 = Path('insight/matrix.csv')  
 filepath1.parent.mkdir(parents=True, exist_ok=True)
@@ -26,6 +32,3 @@ filepath2.parent.mkdir(parents=True, exist_ok=True)
 
 covMatrix.to_csv(filepath1)
 eR.to_csv(filepath2)
-
-print(covMatrix)
-print(eR)
