cb0094cc7574b359fa7816da850a06b1e3517fd2
JoshOberst <joshoberst11@gmail.com>
2022-03-03 20:35:31 -0500

    E[R] and printing

--- insight.py
+++ insight.py
@@ -2,9 +2,11 @@ import yfinance as yf
 from yahoofinancials import YahooFinancials
 import pandas as pd
 import datetime as dt
+from pathlib import Path 
 
-startDay = dt.datetime(2022,1,31)
-stockList = ['SPY','AAPL','TSLA']
+
+startDay = dt.datetime(2018,12,31)
+stockList = ['AAPL','TSLA','DKNG','BB','GME','WISH','PARA','META']
 
 def getReturns(dataStore):
     global startDay
@@ -25,6 +27,17 @@ data = yf.download(stockList,startDay).get("Adj Close")
 
 returns = getReturns(data).transpose()
 covMatrix = returns.reset_index(drop=True).cov()
+eR = (returns.copy().sum())/returns.size #daily
 
+filepath1 = Path('insight/matrix.csv')  
+filepath1.parent.mkdir(parents=True, exist_ok=True)
 
+filepath2 = Path('insight/er.csv')  
+filepath2.parent.mkdir(parents=True, exist_ok=True)
+
+covMatrix.to_csv(filepath1)
+eR.to_csv(filepath2)
+
+print(covMatrix)
+print(eR)
 
